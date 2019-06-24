f92d01cebc5c3e971f377133e563b3e394b1acf3
Vlad <pokidin@ram.com.ua>
2019-06-24 17:30:48 +0300

    init

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,64 +1,45 @@
-import React, { Component } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Link } from "react-router-dom";
-import classNames from "classnames";
+import React, { Component } from 'react';
 import { withStyles } from "@material-ui/styles";
+import classNames from "classnames";
 import styles from "./styles/ColorBoxStyles";
+import './ColorBox.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { copied: false };
-    this.changeCopyState = this.changeCopyState.bind(this);
-  }
-  changeCopyState() {
-    this.setState({ copied: true }, () => {
-      setTimeout(() => this.setState({ copied: false }), 1500);
-    });
-  }
 
-  render() {
-    const {
-      name,
-      background,
-      moreUrl,
-      showingFullPalette,
-      classes
-    } = this.props;
+	state = {
+		copied: false
+	}
 
-    const { copied } = this.state;
-    return (
-      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className={classes.ColorBox}>
-          <div
-            style={{ background }}
-            className={classNames(classes.copyOverlay, {
-              [classes.showOverlay]: copied
-            })}
-          />
+	changeCopyState = () => {
+		this.setState({ copied: true },() => {
+			setTimeout(() => this.setState({ copied: false }), 1500);
+		})
+	}
 
-          <div
-            className={classNames(classes.copyMessage, {
-              [classes.showMessage]: copied
-            })}
-          >
-            <h1>copied!</h1>
-            <p className={classes.copyText}>{background}</p>
-          </div>
-          <div>
-            <div className={classes.boxContent}>
-              <span className={classes.colorName}>{name}</span>
-            </div>
-            <button className={classes.copyButton}>Copy</button>
-          </div>
-          {showingFullPalette && (
-            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-              <span className={classes.seeMore}>MORE</span>
-            </Link>
-          )}
-        </div>
-      </CopyToClipboard>
-    );
-  }
+	render() {
+		const { name, background } = this.props;
+		return (
+			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
+				<div style={{ background: background }} className="ColorBox">
+				<div style={{ background: background }} className={`copy-overlay ${this.state.copied ? "show" : ""}`} ></div>
+				<div  className={`copy-msg ${this.state.copied ? "show" : ""}`}>
+					<h1>Copied!</h1>
+					<p>{background}</p>
+				</div>
+					<div className="copy-container">
+						<div className="box-content">
+							<span>
+								{name}
+							</span>
+						</div>
+						<button className="copy-button">Copy</button>
+					</div>
+					<span className="see-more">More</span>
+				</div>
+			</CopyToClipboard>
+		)
+	}
 }
+
 export default withStyles(styles)(ColorBox);
--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,47 +1,45 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import Navbar from "./Navbar";
-import PaletteFooter from "./PaletteFooter";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/PaletteStyles";
+import './Palette.css'
+import uuid from 'uuid';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { level: 500, format: "hex" };
-    this.changeLevel = this.changeLevel.bind(this);
-    this.changeFormat = this.changeFormat.bind(this);
-  }
-  changeLevel(level) {
-    this.setState({ level });
-  }
-  changeFormat(val) {
-    this.setState({ format: val });
-  }
+
+	state = {
+		level: 500
+	}
+
+	changeLevel = (level) => {
+		this.setState({ level })
+	}
+
   render() {
-    const { colors, paletteName, emoji, id } = this.props.palette;
-    const { classes } = this.props;
-    const { level, format } = this.state;
-    const colorBoxes = colors[level].map(color => (
-      <ColorBox
-        background={color[format]}
-        name={color.name}
-        key={color.id}
-        moreUrl={`/palette/${id}/${color.id}`}
-        showingFullPalette
-      />
-    ));
+
+		const colorBoxes = this.props.palette.colors[this.state.level].map(color => (
+			<ColorBox background={color.hex} name={color.name} key={uuid.v4()} />
+		));
+		
+
+
     return (
-      <div className={classes.Palette}>
-        <Navbar
-          level={level}
-          changeLevel={this.changeLevel}
-          handleChange={this.changeFormat}
-          showingAllColors
-        />
-        <div className={classes.colors}>{colorBoxes}</div>
-        <PaletteFooter paletteName={paletteName} emoji={emoji} />
-      </div>
+      <div className="Palette">
+				<div className="slider">
+					<Slider 
+						defaultValue={this.state.level} 
+						min={100} 
+						max={900} 
+						step={100}
+						onAfterChange={this.changeLevel} />
+				</div>
+				<div className="Palette-colors">
+					{colorBoxes}
+				</div>
+				
+			</div>
     );
   }
 }
