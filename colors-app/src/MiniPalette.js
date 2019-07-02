70474ca424bc3ad04e395c1d27f7b278a1004b59
Vlad <pokidin@ram.com.ua>
2019-07-02 16:18:24 +0300

    new new

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -2,18 +2,64 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-	main: {
-		backgroundColor: "purple",
-		border: "3px solid teal"
+	root: {
+		backgroundColor: "white",
+		borderRadius: "5px",
+		padding: "0.5rem",
+		position: "relative",
+		overflow: "hidden",
+		"&:hover": {
+			cursor: "pointer"
+		}
+	},
+	colors: {
+		backgroundColor: "grey",
+		height: "150px",
+		width: "100%",
+		borderRadius: "5px",
+		overflow: "hidden"
+	}, 
+	title: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		margin: "0",
+		color: "black",
+		paddingTop: "0.5rem",
+		fontSize: "1rem",
+		position: "relative"
+	},
+	emoji: {
+		marginLeft: "0.5rem",
+		fontSize: "1.5rem"
+	},
+	miniColor: {
+		height: "25%",
+		width: "20%",
+		display: "inline-block",
+		margin: "0 auto",
+		position: "relative",
+		marginBottom: "-4px"
 	}
 }
 
 function MiniPalette(props) {
-	const { classes } = props;
-	console.log(classes)
+	const { classes, paletteName, emoji, colors } = props;
+	const miniColorBoxes = colors.map(color => (
+		<div 
+			className={classes.miniColor} 
+			style={{backgroundColor: color.color}} 
+			key={color.name}
+		/>
+	))
 	return (
-		<div className={classes.main}>
-			<h2>Mini</h2>
+		<div className={classes.root} onClick={props.handleClick}>
+			<div className={classes.colors}>
+				{miniColorBoxes}
+			</div>
+			<h5 className={classes.title}>
+				{paletteName} <span className={classes.emoji}>{emoji}</span>
+			</h5>
 		</div>
 	)
 }
